62cd22f573dfebf3494fe749495496e1f0ccf90d
Susanna Weber <s_weber@berkeley.edu>
2022-12-02 11:09:24 -0800

    working color detection w LED actuation

--- esp_serial_blink/esp_serial_blink.ino
+++ esp_serial_blink/esp_serial_blink.ino
@@ -1,26 +1,14 @@
 const int ledPin = 12; // pin the LED is attached to
 int incomingByte;      // variable stores  serial data
-
+int x;
 void setup() {
   // initialize serial communication:
-  Serial.begin(9600);
-  // initialize the LED pin as an output:
-  pinMode(ledPin, INPUT);
-  digitalWrite(ledPin, LOW);
+  Serial.begin(115200);
+  Serial.setTimeout(1);
 }
 
 void loop() {
-  // see if there's incoming serial data:
-  if (Serial.available() > 0) {
-    // read the oldest byte in the serial buffer:
-    incomingByte = Serial.read();
-    // if it's a capital H (ASCII 72), turn on the LED:
-    if (incomingByte == '1') {
-      digitalWrite(ledPin, HIGH);
-    }
-    // if it's an L (ASCII 76) turn off the LED:
-    if (incomingByte == '0') {
-      digitalWrite(ledPin, LOW);
-    }
-  }
+  while (!Serial.available());
+  x = Serial.readString().toInt();
+  delay(100);
 }
--- esp_serial_com.py
+++ esp_serial_com.py
@@ -1,32 +1,18 @@
 import serial
 import time
 
-ser = serial.Serial('COM4', 115200,  timeout=.1)
-# ser.baudrate = 115200
-#ser.port = 'COM4'
-ser.close()
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.port = 'COM4'
 ser.open()
-print(ser.name)
 
-def write_read(x):
-    ser.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
-    data = ser.readline()
-    return data
+values = bytearray([4, 9, 62, 144, 56, 30, 147, 3, 210, 89, 111, 78, 184, 151, 17, 129])
+ser.write(values)
 
-while True:
-    num = input("Enter a number: ") # Taking input from user1
-    value = write_read(num)
-    print(value) # printing the value
+total = 0
 
-
-# values = bytearray([101, 202, 101])
-# ser.write(values)
-
-# total = 0
-
-# while total < len(values):
-#      print(ord(ser.read(1)))
-#      total=total+1
+while total < len(values):
+    print(ord(ser.read(1)))
+    total=total+1
     
 ser.close()
