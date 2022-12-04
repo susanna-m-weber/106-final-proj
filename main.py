907e6bfd8104f622f31cc6612e86feef45012918
PranitMohnot <pranit_mohnot@berkeley.edu>
2022-12-03 16:22:33 -0800

    added main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import serial
+import time
+import serial
+from color_detect import *
+
+if __name__ == "__main__":
+    main()
+
+
+def main():
+    pos_vals = np.zeros(5)
+
+
+    try:
+        while True:
+            pos_vals = np.vstack(color_detect(), pos_vals)  #call the vision code, add current ball x,
+                                                            #ball y, goalie x, goalie y, and time.
+            ballx = pos_vals[0:4, 0]
+            bally = pos_vals[0:4, 1]
+            time = pos_vals[0:4, 4]
+
+            (x_coeff1, x_coeff0) = regress(time, ballx)
+
+
+
+            currGoalie = pos_vals[0][4]
+            desiredGoalie = 0
+            #send currGoalie and desiredGoalie to ESP32
+
+
+    except KeyboardInterrupt:
+        pass
+
+
+def regress(x, y):
+    # number of observations/points
+    n = np.size(x)
+
+    # mean of x and y vector
+    m_x = np.mean(x)
+    m_y = np.mean(y)
+
+    # calculating cross-deviation and deviation about x
+    SS_xy = np.sum(y * x) - n * m_y * m_x
+    SS_xx = np.sum(x * x) - n * m_x * m_x
+
+    # calculating regression coefficients
+    b_1 = SS_xy / SS_xx
+    b_0 = m_y - b_1 * m_x
+
+    return (b_0, b_1)
+
+
+
